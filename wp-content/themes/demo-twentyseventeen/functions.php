5ffd4cb4f85a5590f4f7e6ff03e5fe016f0667b3
Jonathan Stegall <jstegall@minnpost.com>
2018-04-13 23:34:51 -0500

    plugin/theme update

--- wp-content/themes/demo-twentyseventeen/functions.php
+++ wp-content/themes/demo-twentyseventeen/functions.php
@@ -233,4 +233,155 @@ function twentyseventeen_cmb2_settings() {
 		'type'             => 'text',
 	) );
 
+	$app_settings = new_cmb2_box( array(
+		'id'            => 'App',
+		'title'         => __( 'App Settings', 'twentyseventeen_child' ),
+		'object_types'  => array( 'app' ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'low',
+		'show_names'    => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // Keep the metabox closed by default
+	) );
+	$app_settings->add_field( array(
+		'name'             => 'First Name 2',
+		'desc'             => '',
+		'id'               => '_first_name2',
+		'type'             => 'text',
+	) );
+
+	$app_settings->add_field( array(
+		'name'             => 'Last Name 2',
+		'desc'             => '',
+		'id'               => '_last_name2',
+		'type'             => 'text',
+	) );
+
+	$app_settings->add_field( array(
+		'name'             => 'Company 2',
+		'desc'             => '',
+		'id'               => '_company2',
+		'type'             => 'text',
+	) );
+
+	$donation_settings = new_cmb2_box( array(
+		'id'            => 'Donation',
+		'title'         => __( 'Donation Settings', 'twentyseventeen_child' ),
+		'object_types'  => array( 'donation' ), // Post type
+		'context'       => 'normal',
+		'priority'      => 'low',
+		'show_names'    => true, // Show field names on the left
+		// 'cmb_styles' => false, // false to disable the CMB stylesheet
+		// 'closed'     => true, // Keep the metabox closed by default
+	) );
+	$donation_settings->add_field( array(
+		'name'       => 'Close Date',
+		'id'         => '_close_date',
+		'type'       => 'text_date',
+		'desc'       => '',
+	) );
+	$donation_settings->add_field( array(
+		'name'             => 'Stage',
+		'desc'             => '',
+		'id'               => '_stage',
+		'type'             => 'select',
+		'show_option_none' => true,
+		'options'          => array(
+			'Prospecting' => __( 'Prospecting', 'twentyseventeen_child' ),
+			'Cultivating'   => __( 'Cultivating', 'twentyseventeen_child' ),
+			'Solicited'     => __( 'Solicited', 'twentyseventeen_child' ),
+			'Pledged'     => __( 'Pledged', 'twentyseventeen_child' ),
+			'Closed Won'     => __( 'Closed Won', 'twentyseventeen_child' ),
+			'Closed Lost'     => __( 'Closed Lost', 'twentyseventeen_child' ),
+			'Refunded'     => __( 'Refunded', 'twentyseventeen_child' ),
+		),
+	) );
+	$donation_settings->add_field( array(
+		'name'             => 'Type',
+		'desc'             => '',
+		'id'               => '_type',
+		'type'             => 'select',
+		'show_option_none' => false,
+		'default'          => 'Donation',
+		'options'          => array(
+			'Donation' => __( 'Donation', 'twentyseventeen_child' ),
+			'Grant'   => __( 'Grant', 'twentyseventeen_child' ),
+			'Sponsorship'     => __( 'Sponsorship', 'twentyseventeen_child' ),
+			'Sales'     => __( 'Sales', 'twentyseventeen_child' ),
+		),
+	) );
+	$donation_settings->add_field( array(
+		'name'             => 'Subtype',
+		'desc'             => '',
+		'id'               => '_subtype',
+		'type'             => 'select',
+		'show_option_none' => true,
+		'options'          => array(
+			'Donation: Individual' => __( 'Donation: Individual', 'twentyseventeen_child' ),
+			'Donation: Organization' => __( 'Donation: Organization', 'twentyseventeen_child' ),
+			'Donation: In-kind' => __( 'Donation: In-kind', 'twentyseventeen_child' ),
+			'Grant: Grant'   => __( 'Grant: Grant', 'twentyseventeen_child' ),
+			'Sponsorship: Event (individual)'     => __( 'Sponsorship: Event (individual)', 'twentyseventeen_child' ),
+			'Sponsorship: Event (organization)'     => __( 'Sponsorship: Event (organization)', 'twentyseventeen_child' ),
+			'Sales: Advertising'     => __( 'Sales: Advertising', 'twentyseventeen_child' ),
+			'Sales: Merchandise'     => __( 'Sales: Merchandise', 'twentyseventeen_child' ),
+			'Sales: Tickets'     => __( 'Sales: Tickets', 'twentyseventeen_child' ),
+			'Sales: Auction'     => __( 'Sales: Auction', 'twentyseventeen_child' ),
+			'Sales: Other'     => __( 'Sales: Other', 'twentyseventeen_child' ),
+		),
+	) );
+	$donation_settings->add_field( array(
+		'name'       => 'Amount',
+		'id'         => '_amount',
+		'type'       => 'text',
+		'desc'       => '',
+	) );
+	// primary campaign source?
+	$donation_settings->add_field( array(
+		'name'             => 'Payment Type',
+		'desc'             => '',
+		'id'               => '_payment_type',
+		'type'             => 'select',
+		'show_option_none' => true,
+		'options'          => array(
+			'Stripe' => __( 'Stripe', 'twentyseventeen_child' ),
+			'Acceptiva'   => __( 'Acceptiva', 'twentyseventeen_child' ),
+			'Givalike'     => __( 'Givalike', 'twentyseventeen_child' ),
+			'Check'     => __( 'Check', 'twentyseventeen_child' ),
+			'Cash'     => __( 'Cash', 'twentyseventeen_child' ),
+			'ACH'     => __( 'ACH', 'twentyseventeen_child' ),
+			'Goods'     => __( 'Goods', 'twentyseventeen_child' ),
+			'Razoo'     => __( 'Razoo', 'twentyseventeen_child' ),
+			'Services'     => __( 'Services', 'twentyseventeen_child' ),
+			'Stocks'     => __( 'Stocks', 'twentyseventeen_child' ),
+			'Paypal'     => __( 'Paypal', 'twentyseventeen_child' ),
+			'BidPal'     => __( 'BidPal', 'twentyseventeen_child' ),
+			'GiveMN.org'     => __( 'GiveMN.org', 'twentyseventeen_child' ),
+			'Ticketmaster'     => __( 'Ticketmaster', 'twentyseventeen_child' ),
+			'Eventbrite'     => __( 'Eventbrite', 'twentyseventeen_child' ),
+			'Cowles Center processor'     => __( 'Cowles Center processor', 'twentyseventeen_child' ),
+		),
+	) );
+
+	$donation_settings->add_field( array(
+		'name'             => 'First Name',
+		'desc'             => '',
+		'id'               => '_first_name',
+		'type'             => 'text',
+	) );
+
+	$donation_settings->add_field( array(
+		'name'             => 'Last Name',
+		'desc'             => '',
+		'id'               => '_last_name',
+		'type'             => 'text',
+	) );
+
+	$donation_settings->add_field( array(
+		'name'             => 'Email Address',
+		'desc'             => '',
+		'id'               => '_email_address',
+		'type'             => 'text',
+	) );
+
 }
